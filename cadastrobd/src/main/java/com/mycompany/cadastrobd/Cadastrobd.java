73d744682acb095c5979bbdb4c16dd9630e0cd63
Reginaldo Campelo <regin.developer@gmail.com>
2024-08-27 10:44:39 -0300

    adding base project

---
+++ cadastrobd/src/main/java/com/mycompany/cadastrobd/Cadastrobd.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastrobd;
+
+/**
+ *
+ * @author regin
+ */
+public class Cadastrobd {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
